871abbffc2c19e5270623cd290ceadda76cd2147
Taylor <taylorletsoaka@gmail.com>
2018-08-22 16:35:27 +0200

    Update prime.py

--- Lab 2 exercise/prime.py
+++ Lab 2 exercise/prime.py
@@ -13,7 +13,48 @@ def PrimeNumbers(max):
         if(prime):
             print(number)
 
+def prime_numbers(max):
+
+    N = 0
+    numbers = []
+    marked = [False]*max
+    print(len(marked))
+    if(max < 2):
+        print("No primes")
+    elif (max == 2):
+        numbers.append(2)
+    else:
+        numbers = range(max+1)
+        index = 2
+        constant = numbers[index]
+        variable = numbers[index]
+        product = constant*variable
+
+        while(constant*constant < max):
+
+            if(product < max):
+                marked[product] = True
+                variable += 1
+                product = constant*variable
+
+            if(product >= max):
+                for i in range(constant+1, max):
+                    if(marked[i] == False):
+                        constant = numbers[i]
+                        break
+                variable = constant
+                product = constant*variable
+
+        primes = []
+        for i in range(2, len(marked)):
+            if marked[i] == False:
+                primes.append(numbers[i])
+
+        print(primes)
 if __name__ == '__main__':
 
     max = input("Enter upper bound:")
-    PrimeNumbers(max)
+    #PrimeNumbers(max)
+
+    prime_numbers(max)
+
